67c1f62ce180cc87d026dc6450174d26d796499d
James Sutcliffe <jamessutcliffe2012@hotmail.co.uk>
2023-01-14 19:59:55 +0000

    Project complete - write readme

--- assets/script.js
+++ assets/script.js
@@ -88,90 +88,81 @@ var upperCasedCharacters = [
   'Z'
 ];
 
+// Global variables logging prompt answers
+var lengthValue = 0;
+var lowerValue = 0;
+var upperValue = 0;
+var numValue = 0;
+var specialValue = 0;
+
 // Function to prompt user for password options
-let lengthValue = 0;
-let lowerValue = 0;
-let upperValue = 0;
-let numValue = 0;
-let specialValue = 0;
-
-// let lower = prompt('Would you like lowercase? enter y or n');
-// let upper = prompt('Would you like uppercase? enter y or n');
-
 function getPasswordOptions() {
-  let length = prompt('Please select a Password length between 10 & 64?');
+    var length = prompt('Please select a Password length between 10 & 64!');
   if (length >= 10 && length <= 64){
     lengthValue = length;
-    let lower = prompt('Would you like lowercase? enter y or n');
+    var lower = prompt('Would you like lowercase? enter y or n');
     lowerValue = lower;
   } else {
-    let length = prompt('Please select a Password length between 10 & 64?');
-  };
+    alert('WARNING! if you did not enter prompt values as requested your password will not be generated correctly! Please refresh and restart if so');
+   };
   if (lowerValue === 'y' || lowerValue === 'n') {
-    let upper = prompt('Would you like uppercase? enter y or n');
+    var upper = prompt('Would you like uppercase? enter y or n');
     upperValue = upper;
    } else {
-      let lower = prompt('Would you like lowercase? enter y or n');
+    alert('WARNING! if you did not enter prompt values as requested your password will not be generated correctly! Please refresh and restart if so');
    };
     if (upperValue === 'y' || upperValue === 'n') {
-    let numeric = prompt('Would you like numbers? enter y or n');
+    var numeric = prompt('Would you like numbers? enter y or n');
     numValue = numeric;
     } else {
-      let upper = prompt('Would you like uppercase? enter y or n');
+      alert('WARNING! if you did not enter prompt values as requested your password will not be generated correctly! Please refresh and restart if so');
     };
   if (numValue === 'y' || numValue === 'n') {
-    let special = prompt('Would you like special characters? enter y or n');
+    var special = prompt('Would you like special characters? enter y or n');
     specialValue = special;
   } else {
-    let numeric = prompt('Would you like numbers? enter y or n');
-  };
-  if (specialValue === 'y' || specialValue === 'n') {
-    specialValue = special;
-  } else {
-    let special = prompt('Would you like special characters? enter y or n');
-}};
-
-
-function test (){
-console.log(lengthValue);
-console.log(lowerValue);
-console.log(upperValue);
-console.log(numValue);
-console.log(specialValue);
+    alert('WARNING! if you did not enter prompt values as requested your password will not be generated correctly! Please refresh and restart if so');
 }
+alert('WARNING! if you did not enter prompt values as requested your password will not be generated correctly! Please refresh and restart if so');
+};
 
 // Function for getting a random element from an array
-
-  // https://developer.mozilla.org/en-US/docs/Web/API/Crypto/getRandomValues
-
-function getRandom(arr) {
-// get a random item from Array
-
-}
+  function getRandom(arr) {
+    const randomNumber = Math.floor(Math.random() * arr.length);
+    const randomItem = arr[randomNumber];
+    return randomItem;
+};
 
 // Function to generate password with user input
+let passwordArray = [];
 function generatePassword() {
-// passwordArray = [];
-//   while passwordArray <= lengthValue {
-//     if value variable === y {
-//       do getRandom(relevantArr)
-//     }
-// push to passwordArray;
-// return passwordArray
+do {
+  if (lowerValue === 'y' && passwordArray.length < lengthValue){
+passwordArray.push(getRandom(lowerCasedCharacters));  } 
+  if (upperValue === 'y' && passwordArray.length < lengthValue){
+passwordArray.push(getRandom(upperCasedCharacters));  } 
+if (numValue === 'y' && passwordArray.length < lengthValue){
+  passwordArray.push(getRandom(numericCharacters));  } 
+  if (specialValue === 'y' && passwordArray.length < lengthValue){
+    passwordArray.push(getRandom(specialCharacters));  } 
+} while (passwordArray.length < lengthValue);
+// Below code randomises the built array so we do not get the prompted values in order of selection
+passwordArray.sort(() => Math.random() - 0.5);
 
-}
+return passwordArray.join('');
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  test ();
   getPasswordOptions();
+  generatePassword();
   var password = generatePassword();
   var passwordText = document.querySelector('#password');
-
   passwordText.value = password;
+  passwordArray = [];
 }
 
 // Add event listener to generate button
